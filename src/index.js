a8ecde0e35fa7b7a2e9e3a9813a61706893d297d
yablochkov <dima5544@yandex.ru>
2021-03-13 21:15:01 +0300

    fix: update syntax

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 
 exports.min = function min (array) {
-  if (array === undefined || array.length === 0) return 0;
+  if (!array || array.length === 0) return 0;
   return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  if (array === undefined || array.length === 0) return 0;
+  if (!array || array.length === 0) return 0;
   return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  if (array === undefined || array.length === 0) return 0;
+  if (!array || array.length === 0) return 0;
   let sum = 0;
   for (let number of array) {
     sum += number;
