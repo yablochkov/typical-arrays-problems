940a4589e744ef04aaf36568893554891f32063d
yablochkov <dima5544@yandex.ru>
2021-03-13 21:12:09 +0300

    feat: add no params test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  let sum = 0;
+  for (let number of array) {
+    sum += number;
+  }
+return sum/array.length;
+  
 }
